4d6f22cda4db09a1435c41610851881513bd14db
Lars Peter Thomsen <larsp@magenta.dk>
2017-07-01 12:15:10 +0200

    Update service classes to new service pattern

--- src/main/java/dk/magenta/datafordeler/gladdrreg/data/address/AddressEntityService.java
+++ src/main/java/dk/magenta/datafordeler/gladdrreg/data/address/AddressEntityService.java
@@ -4,17 +4,14 @@ import dk.magenta.datafordeler.core.exception.AccessDeniedException;
 import dk.magenta.datafordeler.core.exception.AccessRequiredException;
 import dk.magenta.datafordeler.core.fapi.FapiService;
 import dk.magenta.datafordeler.core.user.DafoUserDetails;
-import org.springframework.stereotype.Component;
-
-import javax.jws.WebService;
-import javax.ws.rs.Path;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by lars on 19-05-17.
  */
-@Path("")
-@Component
-@WebService
+@Controller
+@RequestMapping("/gladdrreg/address/1/rest")
 public class AddressEntityService extends FapiService<AddressEntity, AddressQuery> {
 
     @Override
