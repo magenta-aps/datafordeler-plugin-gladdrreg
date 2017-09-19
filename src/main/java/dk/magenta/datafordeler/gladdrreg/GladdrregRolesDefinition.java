4224da57e95c21082bc86e753be07b7e8a7f5705
Lars Peter Thomsen <larsp@magenta.dk>
2017-09-19 13:58:46 +0200

    Add missing role definitions

--- src/main/java/dk/magenta/datafordeler/gladdrreg/GladdrregRolesDefinition.java
+++ src/main/java/dk/magenta/datafordeler/gladdrreg/GladdrregRolesDefinition.java
@@ -28,12 +28,36 @@ public class GladdrregRolesDefinition extends RolesDefinition {
             )
     );
 
+    public static ReadCommandRole READ_GLADDRREG_PULL_ROLE = new ReadCommandRole(
+            "Pull",
+            new HashMap<String, Object>() {{
+                put("plugin", "gladdrreg");
+            }},
+            new ReadCommandRoleVersion(
+                    1.0f,
+                    "Role that gives access to read the status of the PULL command for Gladdrreg data"
+            )
+    );
+
+    public static StopCommandRole STOP_GLADDRREG_PULL_ROLE = new StopCommandRole(
+            "Pull",
+            new HashMap<String, Object>() {{
+                put("plugin", "gladdrreg");
+            }},
+            new StopCommandRoleVersion(
+                    1.0f,
+                    "Role that gives access to stop the PULL command for Gladdrreg data"
+            )
+    );
+
 
     public List<SystemRole> getRoles() {
         ArrayList<SystemRole> roles = new ArrayList<>();
 
         roles.add(READ_GLADDRREG_ROLE);
         roles.add(EXECUTE_GLADDRREG_PULL_ROLE);
+        roles.add(READ_GLADDRREG_PULL_ROLE);
+        roles.add(STOP_GLADDRREG_PULL_ROLE);
 
         return roles;
     }
